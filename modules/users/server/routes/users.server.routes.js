eb474ba635c46ae89a92c0f594c4e59bbfae5133
unknown <datle2>
2017-11-20 23:57:38 +0700

    add route

--- modules/users/server/routes/users.server.routes.js
+++ modules/users/server/routes/users.server.routes.js
@@ -5,4 +5,10 @@ var users = require('../controllers/users.server.controller');
 module.exports = function(app) {
   app.route('/helloworld')
     .get(users.helloworld);
+
+  app.route('/get-user')
+    .get(users.getUser);
+	
+  app.route('/create-user')
+    .post(users.createUser);
 };
